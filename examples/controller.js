40106ca51deacf78c0c5312811753e0b0d93e592
Michal Kuklis <michal.kuklis@gmail.com>
2014-03-13 00:06:24 -0400

    added examples

---
+++ examples/controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var app = angular.module('app', ['ngPersistance']);
+
+phonecatApp.controller('TodosController', function($scope, Todos) {
+  
+  Todos.all().then(function (todos) {
+    $scope.todos = todos;
+  });
+  
+  Todos.save({ name: 'todo 1', done: false });
+  Todos.query.limit(10).run().then(function (todos) {
+  });
+  
+});
